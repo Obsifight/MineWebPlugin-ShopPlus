bb41d1913e143cfedf9119bb3bcff066f3e3bd56
Valentin <contact@eywek.fr>
2017-02-24 17:27:02 +0100

    add hipay wallet to view

--- Config/routes.php
+++ Config/routes.php
@@ -9,4 +9,11 @@ Router::connect('/shop/credits/paymill/payment/create', array('controller' => 'p
 Router::connect('/admin/shop/credits/paymill/config', array('controller' => 'paymill', 'action' => 'config', 'plugin' => 'ShopPlus', 'admin' => true));
 Router::connect('/admin/shop/credits/paymill/history', array('controller' => 'paymill', 'action' => 'get_histories', 'plugin' => 'ShopPlus', 'admin' => true));
 
+Router::connect('/shop/credits/hipay-wallet/payment/ipn', array('controller' => 'HipayWallet', 'action' => 'ipn', 'plugin' => 'ShopPlus'));
+Router::connect('/shop/credits/hipay-wallet/payment/success', array('controller' => 'HipayWallet', 'action' => 'success', 'plugin' => 'ShopPlus'));
+Router::connect('/shop/credits/hipay-wallet/payment/error', array('controller' => 'HipayWallet', 'action' => 'error', 'plugin' => 'ShopPlus'));
+Router::connect('/shop/credits/hipay-wallet/payment/cancel', array('controller' => 'HipayWallet', 'action' => 'cancel', 'plugin' => 'ShopPlus'));
+Router::connect('/admin/shop/credits/hipay-wallet/config', array('controller' => 'HipayWallet', 'action' => 'config', 'plugin' => 'ShopPlus', 'admin' => true));
+Router::connect('/admin/shop/credits/hipay-wallet/history', array('controller' => 'HipayWallet', 'action' => 'get_histories', 'plugin' => 'ShopPlus', 'admin' => true));
+
 Router::connect('/admin/shop/stats', array('controller' => 'ShopStats', 'action' => 'index', 'plugin' => 'ShopPlus', 'admin' => true));
--- Controller/PaymentPageController.php
+++ Controller/PaymentPageController.php
@@ -1,6 +1,71 @@
 <?php
 class PaymentPageController extends ShopPlusAppController {
 
+  private function __encryptHipayWallet($source, $privateKey) {
+    $maxLength = 117;
+    $output = "";
+    while ($source) {
+      $slice = substr($source, 0, $maxLength);
+      $source = substr($source, $maxLength);
+      openssl_private_encrypt($slice, $encrypted, $privateKey);
+      $output .= $encrypted;
+    }
+    return $output;
+  }
+  private function __signHipayWallet($data, $privateKey) {
+    $output = "";
+    openssl_private_encrypt(sha1($data), $output, $privateKey);
+    return $output;
+  }
+
+  /*
+    $offer = array('user_account_id', 'private_key', 'credits', 'id', 'amount')
+  */
+  private function __generateHipayWalletForm($offer) {
+    $xml = "<?xml version='1.0' encoding='utf-8' ?>"
+      . "<order>"
+        . "<userAccountId>".$offer['user_account_id']."</userAccountId>"
+        . "<currency>EUR</currency>"
+        . "<label>".$offer['credits']." ".$this->Configuration->getMoneyName()."</label>"
+        . "<ageGroup>ALL</ageGroup>"
+        . "<categoryId>251</categoryId>"
+        . "<urlAcquital><![CDATA[".Router::url(array('controller' => 'HipayWallet', 'action' => 'ipn', 'plugin' => 'ShopPlus'), true)."]]></urlAcquital>"
+        . "<urlOk><![CDATA[".Router::url(array('controller' => 'HipayWallet', 'action' => 'success', 'plugin' => 'ShopPlus'), true)."]]></urlOk>"
+        . "<urlKo><![CDATA[".Router::url(array('controller' => 'HipayWallet', 'action' => 'error', 'plugin' => 'ShopPlus'), true)."]]></urlKo>"
+        . "<urlCancel><![CDATA[".Router::url(array('controller' => 'HipayWallet', 'action' => 'cancel', 'plugin' => 'ShopPlus'), true)."]]></urlCancel>"
+        . "<urlInstall><![CDATA[".Router::url($this->here, true)."]]></urlInstall>"
+        . "<urlLogo><![CDATA[".Router::url('/theme/Obsifight/img/logo.png', true)."]]></urlLogo>"
+        . "<!-- optional -->"
+        //. "<thirdPartySecurity>compatible</thirdPartySecurity>"
+        . "<locale>fr_FR</locale>"
+        //. "<issuerAccountLogin>".$offer['data']->issue_email."</issuerAccountLogin>"
+        . "<data>"
+            . "<user>"
+              . "<username>{$this->User->getKey('pseudo')}</username>"
+              . "<id>{$this->User->getKey('id')}</id>"
+            . "</user>"
+            . "<offer>"
+              . "<id>{$offer['id']}</id>"
+            . "</offer>"
+        . "</data>"
+        . "<items>"
+            . "<item id='1'>"
+                . "<name>{$offer['credits']} {$this->Configuration->getMoneyName()}</name>"
+                . "<infos>Achat de {$offer['credits']} {$this->Configuration->getMoneyName()} sur {$this->Configuration->getKey('name')}</infos>"
+                . "<amount>{$offer['amount']}</amount>"
+                . "<categoryId>251</categoryId>"
+                . "<quantity>1</quantity>"
+                . "<reference>REF1</reference>"
+            . "</item>"
+        . "</items>"
+      . "</order>";
+    $xml = trim($xml);
+    $privateKey = $offer['private_key'];
+    $encodedData = base64_encode($this->__encryptHipayWallet(base64_encode($xml), $privateKey));
+    $md5Sign = base64_encode($this->__signHipayWallet(base64_encode($xml), $privateKey));
+    return array($encodedData, $md5Sign);
+  }
+
   public function addCredit() {
     if (!$this->isConnected)
       throw new ForbiddenException();
@@ -49,6 +114,29 @@ class PaymentPageController extends ShopPlusAppController {
       $this->set('paymill', true);
       $this->set('paymillCreditFor1', $paymillConfig['PaymillConfiguration']['credits_for_1']);
       $this->set('paymillPublicKey', $paymillConfig['PaymillConfiguration']['public_key']);
+    } else {
+      $this->set('paymill', false);
+    }
+
+    $this->loadModel('ShopPlus.HipayWalletConfiguration');
+    $hipayWalletConfig = $this->HipayWalletConfiguration->find('first');
+    if (!empty($hipayWalletConfig) && $hipayWalletConfig['HipayWalletConfiguration']['status']) {
+      $this->set('hipayWallet', true);
+      $this->set('hipayWalletWebsiteId', $hipayWalletConfig['HipayWalletConfiguration']['website_id']);
+      $this->set('hipayWalletTestMode', $hipayWalletConfig['HipayWalletConfiguration']['test']);
+      // offers
+      $this->loadModel('ShopPlus.HipayWalletOffer');
+      $hipayWalletOffers = $this->HipayWalletOffer->find('all', array('order' => 'amount DESC'));
+      $this->set('hipayWalletOffers', array_map(function ($offer) use ($hipayWalletConfig) {
+        list($encodedData, $md5Sign) = $this->__generateHipayWalletForm(array(
+          'user_account_id' => $hipayWalletConfig['HipayWalletConfiguration']['user_account_id'],
+          'private_key' => $hipayWalletConfig['HipayWalletConfiguration']['private_key'],
+          'credits' => $offer['HipayWalletOffer']['credits'],
+          'id' => $offer['HipayWalletOffer']['id'],
+          'amount' => $offer['HipayWalletOffer']['amount']
+        ));
+        return array('encodedData' => $encodedData, 'md5Sign' => $md5Sign, 'credits' => $offer['HipayWalletOffer']['credits'], 'amount' => $offer['HipayWalletOffer']['amount']);
+      }, $hipayWalletOffers));
     } else {
       $this->set('paymill', false);
     }
