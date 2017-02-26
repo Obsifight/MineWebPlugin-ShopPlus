1303221bc17d43fad5f95b08c22b5f80361b5a19
Valentin <contact@eywek.fr>
2017-02-26 13:12:27 +0100

    edit schema

--- SQL/schema.php
+++ SQL/schema.php
@@ -65,4 +65,42 @@ class ShopPlusAppSchema extends CakeSchema {
     ),
     'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
   );
+
+  public $shopplus__hipay_wallet_configurations = array(
+    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+    'user_account_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+    'website_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+    'private_key' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+    'status' => array('type' => 'boolean', 'null' => false, 'default' => null),
+    'test' => array('type' => 'boolean', 'null' => false, 'default' => null),
+    'indexes' => array(
+      'PRIMARY' => array('column' => 'id', 'unique' => 1)
+    ),
+    'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+  );
+
+  public $shopplus__hipay_wallet_histories = array(
+    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+    'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+    'offer_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+    'amount' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+    'credits' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+    'transaction_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 250, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+    'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+    'indexes' => array(
+      'PRIMARY' => array('column' => 'id', 'unique' => 1)
+    ),
+    'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+  );
+
+  public $shopplus__hipay_wallet_offers = array(
+    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+    'credits' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+    'amount' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+    'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+    'indexes' => array(
+      'PRIMARY' => array('column' => 'id', 'unique' => 1)
+    ),
+    'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+  );
 }
